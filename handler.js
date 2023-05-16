363a546843d5e289b8755abf15ffa14b7e4ba15c
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-16 15:34:29 +0200

    Support for text

--- handler.js
+++ handler.js
@@ -2,10 +2,10 @@ import { input } from "../../public/meta.js";
 import config from "@proxtx/config";
 
 export const evaluate = async (value) => {
-  return eval(`
-    ${await input.resolveArgument(
+  return await eval(`(async () => {
+    return await input.resolveArgument(
       config.pwd,
       value.value1
-    )} ${await input.resolveArgument(config.pwd, value.operator)}
-    ${await input.resolveArgument(config.pwd, value.value2)}`);
+    ) ${await input.resolveArgument(config.pwd, value.operator)}
+    await input.resolveArgument(config.pwd, value.value2)})()`);
 };
