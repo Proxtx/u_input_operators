a2341455edbf03bcfb4ed5af3d17a22205cfb381
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-19 17:34:11 +0200

    Adjust to new apps dependency

--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
-import { input } from "../../public/meta.js";
+import { resolveInput } from "../../public/api.js";
 import config from "@proxtx/config";
 
 export const evaluate = async (value) => {
   return await eval(`(async () => {
-    return await input.resolveArgument(
+    return await resolveInput(
       config.pwd,
       value.value1
-    ) ${await input.resolveArgument(config.pwd, value.operator)}
-    await input.resolveArgument(config.pwd, value.value2)})()`);
+    ) ${await resolveInput(config.pwd, value.operator)}
+    await resolveInput(config.pwd, value.value2)})()`);
 };
